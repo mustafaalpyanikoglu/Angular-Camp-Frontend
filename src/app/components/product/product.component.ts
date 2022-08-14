d76ed8d2bd5870bb096e2208b0c776d4dd21c639
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-08-14 13:51:56 +0300

    feat: ToastrModule added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -16,7 +17,8 @@ export class ProductComponent implements OnInit { //çalışan ilk kod
   filterText = "";
 
   constructor(private productService:ProductService,
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute,
+    private toastrSerivce:ToastrService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>
@@ -47,6 +49,11 @@ export class ProductComponent implements OnInit { //çalışan ilk kod
       console.log(response.data)
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product:Product)
+  {
+    this.toastrSerivce.success("Sepete eklendi.",product.productName)
   }
 
 }
